f5e176e3276672e995c6329649c250881441f185
AlvinBach <quocanh1990bach@gmail.com>
2021-11-16 11:20:41 +0200

    deploy on github pages using git actions

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const store = createStore(
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
     </Router>
   </Provider>,
